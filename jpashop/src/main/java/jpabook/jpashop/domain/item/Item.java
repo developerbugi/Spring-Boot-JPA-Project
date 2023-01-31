12f73b2fa566f8617465089d4ada796a63b0a0ca
거부길 <sopens@naver.com>
2023-01-31 22:38:23 +0900

    상품 도메인 개발
    상품 엔티티 비즈니스 로직 추가, 리포지토리 개발, 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 재고 수량 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수량 감소
+     */
+    public void removeStock(int quantity){
+
+        if(this.stockQuantity - quantity >= 0) {
+            this.stockQuantity -= quantity;
+        }
+
+        else throw new NotEnoughStockException("need more stock");
+    }
 }
