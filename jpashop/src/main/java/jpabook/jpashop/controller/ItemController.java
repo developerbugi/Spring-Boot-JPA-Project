53eb0d1a8c82148cf3c15b9737866a96b700b641
거부길 <sopens@naver.com>
2023-02-05 15:17:08 +0900

    수정관련 오류 수정
    1. 수정시 기존 값이 화면에 뿌려지지 않는 오류 해결
    
    2. URL 매핑 문제 해결

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -45,25 +45,25 @@ public class ItemController {
         return "items/itemList";
     }
 
-    @GetMapping("items/{itemId}/edit")
+    @GetMapping(value = "/items/{itemId}/edit")
     public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
         Book item = (Book) itemService.findOne(itemId);
 
         BookForm form = new BookForm();
-        item.setId(item.getId());
-        item.setName(item.getName());
-        item.setPrice(item.getPrice());
-        item.setStockQuantity(item.getStockQuantity());
-        item.setAuthor(item.getAuthor());
-        item.setIsbn(item.getIsbn());
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
 
         model.addAttribute("form", form);
         return "items/updateItemForm";
     }
 
-    @PostMapping("items/{itemId}/edit")
+    @PostMapping(value = "/items/{itemId}/edit")
     public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form){
         itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
-        return "redirect:items";
+        return "redirect:/items";
     }
 }
