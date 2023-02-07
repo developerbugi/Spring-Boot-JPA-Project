7cb48fbd6c64b9bed3fedb77167875a8662bac8a
거부길 <sopens@naver.com>
2023-02-07 22:02:41 +0900

    회원가입 검증로직 추가

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,14 +3,17 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class MemberForm {
     @NotEmpty(message = "회원 이름은 필수입니다.")
     private String name;
-
+    @NotBlank(message = "도시를 입력해주세요")
     private String city;
+    @NotBlank(message = "거리를 입력해주세요")
     private String street;
+    @NotBlank(message = "우편번호를 입력해주세요")
     private String zipcode;
 }
